f4fd0adc8b600f5012a2d07cfca2bc771be28ae7
RT Long <65727130+rvstwi@users.noreply.github.com>
2026-08-31 14:39:17 -0600

    Add files via upload

--- resources/graph paper.js
+++ resources/graph paper.js
@@ -89,7 +89,10 @@ function watchTranspInput (event) {
 function SetBackground() {
     let c = hexToRgb(gcolor);
     let s = c.r + ", " + c.g + ", " + c.b + ", " + gtransp + "%";
-    let p = `repeating-linear-gradient(90deg, rgba(${s}) 0px, rgba(${s}) 1px, transparent 1px, transparent 20px), repeating-linear-gradient(0deg, rgba(${s}) 0px, rgba(${s}) 1px, transparent 1px, transparent 20px)`;
+    let p = `
+        repeating-linear-gradient(90deg, rgba(${s}) 0px, rgba(${s}) 1px, transparent 1px, transparent 20px), 
+        repeating-linear-gradient(0deg, rgba(${s}) 0px, rgba(${s}) 1px, transparent 1px, transparent 20px)
+    `;
     $(grid).css('background-image', p);
 }
 
